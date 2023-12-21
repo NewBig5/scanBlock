0914571ffc70f532e4657e147e04529a8cd848e6
Your Name <you@example.com>
2023-12-22 00:05:46 +0800

    b

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cfg = require('./config/cfg.js')
 const network = 'Avalanche'
 
 
-
+const burst = 10
 async function main(){
     const url = cfg[network].url
     let web3 = new Web3(url)
@@ -15,13 +15,25 @@ async function main(){
     let fromBlockNumber = lastBlockNumber - 10000
 
     while(true) {
-        let block = await web3.eth.getBlock(fromBlockNumber, true)
-        console.log("block:", block.transactions.length)
-        for(let i=0; i<block.transactions.length; i++) {
-            //console.log("tx:", i, block.transactions[i])
+        let blockPS = []
+        let blocks = []
+        for(let k=0; k<burst; k++) {
+            let blockp =  web3.eth.getBlock(fromBlockNumber, true)
+            fromBlockNumber++
+            blockPS.push(blockp)
         }
-        console.log("time:", parseInt(Date.now()/1000), fromBlockNumber)
-        fromBlockNumber++
+        blocks = await Promise.all(blockPS)
+        for(let k=0; k<burst; k++) {
+            let block = blocks[k]
+            console.log("block:", block.transactions.length)
+            for(let i=0; i<block.transactions.length; i++) {
+                //console.log("tx:", i, block.transactions[i])
+            }
+            console.log("time:", parseInt(Date.now()/1000), block.number)
+        }
+
+
+
     }
 }
 
