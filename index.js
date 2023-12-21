40fb1c333b3aa687e73866e1989b031589689163
Your Name <you@example.com>
2023-12-21 23:48:31 +0800

    b

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ async function main(){
 
     let lastBlockNumber = await web3.eth.getBlockNumber()
     console.log("lastBlockNumber:", lastBlockNumber)
+
+    let fromBlockNumber = lastBlockNumber - 10000
+
+    while(true) {
+        let block = await web3.eth.getBlock(fromBlockNumber, true)
+        console.log("block:", block.transactions.length)
+        for(let i=0; i<block.transactions.length; i++) {
+            //console.log("tx:", i, block.transactions[i])
+        }
+        console.log("time:", parseInt(Date.now()/1000), fromBlockNumber)
+        fromBlockNumber++
+    }
 }
 
 
